08fd6be998432b835c0182f08bf4f3b3a3f10595
samschool789@gmail.com <samschool789@gmail.com>
2022-07-16 19:31:21 -0500

    changes

--- models/Comment.js
+++ models/Comment.js
@@ -3,16 +3,18 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    //set custom id to avoid confusion with parent comment _id
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
-      default: () => Types.ObjectId()
+      default: () => new Types.ObjectId()
     },
     replyBody: {
       type: String,
-    },  
-        writtenBy: {
-      type: String
+      required: true
+    },
+    writtenBy: {
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
