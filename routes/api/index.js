c8052159782689db87748f87a8bdd058004592de
samschool789@gmail.com <samschool789@gmail.com>
2022-07-16 19:20:20 -0500

    idb

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-
 const commentRoutes = require('./comment-routes');
-
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
